d5ab4cf68a5cc34cfeefe1b5b560211c9054b628
jonathanvelasquez <42391288+jonathanvelasquez@users.noreply.github.com>
2023-09-21 12:38:07 -0500

    Project  architecture

---
+++ Sales2023/Sales.Mbile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Mbile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Sales2023/Sales.Mbile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sales.Mbile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
